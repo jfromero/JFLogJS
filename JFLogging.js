ef9fad6d165aa3ecbf51035841faf7675beda9f7
Juan Francisco Romero Moreno <jfromeromoreno@gmail.com>
2017-01-04 15:55:46 +0100

    Initial Version v1

---
+++ JFLogging.js
@@ -0,0 +1,170 @@
+/*
+MIT License
+
+Copyright (c) 2017 Juan Francisco Romero Moreno
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+/*
+JFLogJS  (v1.0 - 2017-)
+
+
+*/
+"use strict";
+if(window.LOG){throw"Attempting to recreate LOG. Library may be included more than once on the page."}
+
+(function() {
+	window.LOG = {
+		// Is log
+		status: null,
+		removeMilSecDate: null,
+		level_all: "ALL",
+		level_debug: "DEBUG",
+		level_info: "INFO",
+		level_warning: "WARN",
+		level_error: "ERROR",
+		level_critical: "CRITICAL",
+		level_nomessages: "NO_MESSAGES",
+		currentLevel: null,
+		// Enabling and disabling logging Library
+		enable: function() {
+			LOG.status = "enabled";
+		},
+		disable: function() {
+			LOG.status = "disabled";
+		},
+		isEnabled: function() {
+			return LOG.status === "enabled";
+		},
+		isDisabled: function() {
+			return LOG.status === "disabled";
+		},
+		setLevel: function(level) {
+			if( level !== LOG.level_all &&
+				level !== LOG.level_debug &&
+				level !== LOG.level_info &&
+				level !== LOG.level_warning &&
+				level !== LOG.level_error &&
+				level !== LOG.level_critical) {
+				throw("Level " + level + " is not allowed. Please use any of these options: " +
+				  LOG.level_all + ", " + LOG.level_debug + ", " + LOG.level_info + ", " + LOG.level_warning + ", " + LOG.level_error + ", " + LOG.level_critical + ", " +  LOG.level_nomessages);
+			}
+			LOG.currentLevel = level;
+		},
+		setAllMessagesLevel: function() {
+				LOG.setLevel(LOG.level_all);
+		},
+		setDebugLevel: function() {
+			LOG.setLevel(LOG.level_debug);
+		},
+		setInfoLevel: function() {
+			LOG.setLevel(LOG.level_info);
+		},
+		setWarningLevel: function() {
+			LOG.setLevel(LOG.level_warning);
+		},
+		setErrorLevel: function() {
+			LOG.setLevel(LOG.level_error);
+		},
+		setCriticalLevel: function() {
+			LOG.setLevel(LOG.level_critical);
+		},
+		setNoMessagesLevel: function() {
+			LOG.setLevel(LOG.level_nomessages);
+		},
+		all: function(msg) {
+			LOG.printMsg(LOG.level_all, msg);
+			launchMsgEvents(LOG.level_all);
+		},
+		debug: function(msg) {
+			LOG.printMsg(LOG.level_debug, msg);
+			LOG.launchMsgEvents(LOG.level_debug);
+		},
+		info: function(msg) {
+			LOG.printMsg(LOG.level_info, msg);
+			LOG.launchMsgEvents(LOG.level_info);
+		},
+		warn: function(msg) {
+			LOG.printMsg(LOG.level_warning, msg);
+			LOG.launchMsgEvents(LOG.level_warning);
+		},
+		error: function(msg) {
+			LOG.printMsg(LOG.level_err, msg);
+			LOG.launchMsgEvents(LOG.level_error);
+		},
+		critical: function(msg) {
+			LOG.printMsg(LOG.level_critical, msg);
+			LOG.launchMsgEvents(LOG.level_critical);
+		},
+		nomessages: function() {
+			LOG.launchMsgEvents(LOG.level_nomessages);
+		},
+		getLevelNumber: function(level) {
+			if(level === LOG.level_all) return 0;
+			if(level === LOG.level_debug) return 1;
+			if(level === LOG.level_info) return 2;
+			if(level === LOG.level_warning) return 3;
+			if(level === LOG.level_error) return 4;
+			if(level === LOG.level_critical) return 10;
+
+			// No messages
+			if(level === LOG.level_nomessages) return 100;
+		},
+		levelIsPrintable: function(levelToTest) {
+			return (LOG.getLevelNumber(levelToTest) >= LOG.getLevelNumber(LOG.currentLevel));
+		},
+		printMsg: function(level, msg) {
+			if(LOG.isEnabled() && LOG.levelIsPrintable(level)) {
+				var d = new Date().toISOString();
+				d = d.replace("T", " ");
+				d = d.replace("Z", "");
+				if() {
+					d = d.substring(0, d.length - 4);
+				}
+
+				console.log(d + " - " + level + " - " + msg);
+			}
+		},
+		onMessageFuncs: [],
+		onMessage: function(msgType, func) {
+			if(!LOG.onMessageFuncs[msgType]) {
+				LOG.onMessageFuncs[msgType] = [];
+			}
+			// If func is not a function, exits
+			if(typeof func !== "function") throw "onMessage Func has to be a function";
+			// Add the function to the queue
+			LOG.onMessageFuncs[msgType].push(func);
+		},
+		launchMsgEvents: function(level) {
+			if(LOG.isEnabled() && LOG.levelIsPrintable(level) && typeof LOG.onMessageFuncs[level] !== "undefined") {
+				for(var i = 0 ; i < LOG.onMessageFuncs[level].length ; i++) {
+					LOG.onMessageFuncs[level][i]();
+				}
+			}
+		},
+	};
+
+	function initLogging() {
+		LOG.enable();
+		LOG.currentLevel = LOG.level_error;
+		LOG.removeMilSecDate = true;
+	}
+	// Init logging:
+	initLogging();
+})();
--- README.md
+++ README.md
@@ -1,1 +1,112 @@
-# JFLogJS
+# JFLogJS (MIT License)
+
+JFLogJS library is a very little Javascript library that allows to make logging easier and better.
+
+
+# Installation
+
+Add this line to your script:
+
+```html
+<script src="JFLogging.js"></script>
+```
+
+NOTE: Change "src" attribute to the real script location.
+
+# Logging
+
+JFLogJS create the "LOG" global object to write logs in the JS console. This object has some attributes and methods very easy to use.
+
+## Enabling and disabling Logging
+
+Check the status:
+
+```javascript
+// It will return "enabled" or "disabled"
+LOG.status
+```
+
+Enable logging:
+
+```javascript
+LOG.enable()
+```
+
+Disable logging:
+```javascript
+LOG.disable()
+```
+
+## Logging levels
+
+Current logging level can be checked with:
+
+```javascript
+LOG.currentLevel
+```
+
+Allowed logging levels are:
+
+- all
+- debug
+- info
+- warning
+- error
+- critical
+- no_messages (no messages are printed)
+
+The principal feature of this library is to print only messages with a level equal or below than currentLevel.
+
+To change current level, use any of these:
+
+```javascript
+LOG.setAllMessagesLevel()	// Change current Level to print all messages
+LOG.setDebugLevel()		// Change current Level to debug
+LOG.setInfoLevel()		// Change current Level to info
+LOG.setWarningLevel() 	// Change current Level to warning
+LOG.setErrorLevel()		// Change current Level to error
+LOG.setCriticalLevel()	// Change current Level to critical
+LOG.setNoMessagesLevel()	// Change current Level to No messages. No messages are printed
+```
+
+## Print messages
+
+Use these commands to logging your messages. Only messages with a level equal or above to current level will be printed:
+
+```javascript
+LOG.all(message)
+LOG.debug(message)
+LOG.info(message)
+LOG.warn(message)
+LOG.error(message)
+LOG.critical(message)
+```
+
+## Removing milliseconds from logging Date
+
+By default milliseconds are removed from message. If you want to print them, use this command:
+
+```javascript
+LOG.removeMilSecDate = false	// show milliseconds on logging
+LOG.removeMilSecDate = true		// remove milliseconds from logging
+```
+
+# Add onMessage events
+
+You can add your custom functions every time a kind of message is printed. Execute:
+
+```javascript
+LOG.onMessage(msgType, func)
+```
+
+Allowed msgType are:
+
+```javacript
+ORQ.level_all
+ORQ.level_debug
+ORQ.level_info
+ORQ.level_warning
+ORQ.level_error
+ORQ.level_critical
+ORQ.level_nomessages
+```
